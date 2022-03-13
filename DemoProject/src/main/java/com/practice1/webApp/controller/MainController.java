71d9d8fa08b548d9b360d995b6e3077088b87466
“piyush_dolai” <piyushdolaispd@gmail.com>
2022-03-13 14:00:17 +0530

    DEmoProjectCommit

---
+++ DemoProject/src/main/java/com/practice1/webApp/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.practice1.webApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/hello")
+	String sayHello() {
+		return "Hello";
+	}
+	
+}
---
+++ DemoProject/src/test/java/com/practice1/webApp/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice1.webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
